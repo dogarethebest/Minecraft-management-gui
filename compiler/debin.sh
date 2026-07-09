554aedf646a0d92823307dd79e7ddcb833cf9b43
dogarethebest <212955596+dogarethebest@users.noreply.github.com>
2026-07-09 18:07:02 +0000

    maybe this would work

--- compiler/debin.sh
+++ compiler/debin.sh
@@ -11,9 +11,27 @@ cp resources/postinst build/DEBIAN/postinst
 cp resources/minecraft-manager.service build/lib/systemd/system/minecraft-manager.service
 cd ..
 
+#!/bin/bash
+
+CONTROL_FILE="./compiler/resources/control"
+
+sed -i -E 's/^Version: ([0-9]+\.[0-9]+\.[0-9]+)-([0-9]+)/echo "Version: \1-$((\2+1))"/e' "$CONTROL_FILE"
+
+echo "Version updated:"
+grep "^Version:" "$CONTROL_FILE"
+PACKAGE=$(grep "^Package:" "$CONTROL_FILE" | awk '{print $2}')
+
+VERSION=$(grep "^Version:" "$CONTROL_FILE" | awk '{print $2}')
+
+ARCH=$(grep "^Architecture:" "$CONTROL_FILE" | awk '{print $2}')
+
+DEB_NAME="${PACKAGE}_${VERSION}_${ARCH}.deb"
+
+echo "$DEB_NAME"
+
 rsync -av --exclude-from=.gitignore --exclude=build --exclude=.git ./ compiler/build/opt/Minecraft-management-gui/
 
 cd compiler
 rm -rf build/opt/Minecraft-management-gui/compiler
 
-dpkg-deb --build build Minecraft-management-gui.deb
+dpkg-deb --build build $DEB_NAME
